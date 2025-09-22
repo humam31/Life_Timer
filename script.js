36a1cc48b82e9c65938e333afeacae206754ed3e
humam31 <humammuslim39@gmail.com>
2025-09-23 00:09:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -37,12 +37,12 @@ const updateAge = () =>{
     const dateDiff= currDate - dobDate;
 
 const year = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365));
-  const month = Math.floor((dateDiff / (1000 * 60 * 60 * 24 * 30)));
-  const week = Math.floor((dateDiff/(1000 * 60 * 60 * 24 * 7 )) );
-  const day = Math.floor(dateDiff / (1000 * 60 * 60 * 24)) % 30;
-  const hour = Math.floor(dateDiff / (1000 * 60 * 60)) % 24;
-  const minute = Math.floor(dateDiff / (1000 * 60)) % 60;
-  const second = Math.floor(dateDiff / 1000) % 60;
+const month = Math.floor((dateDiff % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30));
+const week = Math.floor((dateDiff % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24 * 7));
+const day = Math.floor((dateDiff % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
+const hour = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minute = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+const second = Math.floor((dateDiff % (1000 * 60)) / 1000);
 
   yearEl.innerHTML = makeTwoDigitNumber(year);
   monthEl.innerHTML = makeTwoDigitNumber(month);
